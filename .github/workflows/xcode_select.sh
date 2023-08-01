1db0515009c64f808e5c0dd23a46579b947f52d4
Matt Robinson <mattrob@hey.com>
2023-08-01 16:46:40 -0600

    Initial commit

---
+++ .github/workflows/xcode_select.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+echo "Selecting Xcode for environment"
+
+printenv
+
+sudo xcode-select -p
+sudo xcode-select -s /Applications/Xcode_14.2.app
+
+echo "Generating bazelrc"
+
+# Enable remote cache for all Github Action builds
+echo "build --config=remote_cache" > user.bazelrc
