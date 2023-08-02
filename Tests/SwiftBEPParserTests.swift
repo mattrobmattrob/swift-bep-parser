6b3db39c1b2973f2e79e5643436f5eab63ff2ada
Matt Robinson <mattrob@hey.com>
2023-08-01 19:24:58 -0600

    Add BEP test fixtures (#3)
    Allow the recorded BEP files written via `--build_event_binary_file` to be accessible from within the test’s execution in Xcode/Bazel.
    
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- Tests/SwiftBEPParserTests.swift
+++ Tests/SwiftBEPParserTests.swift
@@ -3,7 +3,28 @@ import XCTest
 @testable import SwiftBEPParser
 
 final class SwiftBEPParserTests: XCTestCase {
-  func test_AlwaysPass() {
-    XCTAssertTrue(SwiftBEPParser.alwaysTrue())
+  private enum InternalError: Error {
+    case missingResourceBundle
+  }
+
+  /// Access file recorded via `--build_event_binary_file`.
+  private func fixtureBuildEventFile(name: String, fileExtension: String = "bep") throws -> String {
+    guard let fixtureBundlePath = Bundle(
+      for: SwiftBEPParserTests.self
+    ).path(forResource: "SwiftBEPParserTestsResources", ofType: "bundle"),
+          let fixtureBundle = Bundle(path: fixtureBundlePath),
+          let resourcePath = fixtureBundle.path(forResource: name, ofType: fileExtension) else {
+      throw InternalError.missingResourceBundle
+    }
+
+    return resourcePath
+  }
+
+  func test_SuccessBEP() throws {
+    XCTAssertNoThrow(try fixtureBuildEventFile(name: "macos_build_success"))
+  }
+
+  func test_FailureBEP() throws {
+    XCTAssertNoThrow(try fixtureBuildEventFile(name: "macos_build_failure"))
   }
 }
